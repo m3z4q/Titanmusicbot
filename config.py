8753da99b809cd0551b161bcd3b1c18b0e6a7dcf
m3z4q <titaneraontop@gmail.com>
2026-07-23 21:53:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "32557753"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "3aec7775e6af24432f2414f941409876" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "8463886318:AAGdmjoz5kbCfz4bu6ZjgToLZzvR8KlGPz4" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
